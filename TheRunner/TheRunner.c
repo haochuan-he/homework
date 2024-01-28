0ba5d2cd2048ce1ae45e9653dc45c67f16096ce8
chuan <1516998446@qq.com>
2024-01-28 21:26:52 +0800

    24.1.28

--- TheRunner/TheRunner.c
+++ TheRunner/TheRunner.c
@@ -12,9 +12,21 @@
 HANDLE consoleHandle; // 操作控制台（也就是那个黑框框）需要的一个变量
 int w, h;             // 高度，宽度，对应 y 和 x
 
+typedef struct Obstacle // 障碍物，数组中1，2，3分别表示左中右道中障碍物y坐标;一列中可以多个
+{
+    int Down[5][3];
+
+} Obstacle;
+
 void Mvaddch(int y, int x, char ch);
 void MvaddchCol(int y, int x, char ch);
-void Runway(int y, int x);
+void MvaddchRow(int y, int x, char ch[]);
+void MvaddchMiddle(int y, int x, int runway, char ch);
+void Display(int y, int x, int man, int score, int speed, Obstacle obstacle);
+void InitObstacle(Obstacle *obstacle);
+void GenerateObstacle(Obstacle *obstacle, int random);
+void MoveObstacle(Obstacle *obstacle, int y);
+
 //
 //
 //
@@ -37,10 +49,11 @@ int main()
                                                // 到这里，闪烁的光标就消失了。
 
     // 游戏数据初始化
-    int speed = 0, score = 0;
+    int speed = 0, score = 0, man = 2;
+    Obstacle obstacle;
+    InitObstacle(&obstacle);
 
     srand(time(0)); // 以当前时间为随机数生成器的种子
-    int random_variable = rand();
     // printf("Random value on [0,%d]: %d\n", RAND_MAX, random_variable);
 
     /********************************************************************************/
@@ -49,13 +62,15 @@ int main()
     /************************************************************************************/
     while (1)
     {
-        Runway(h, w);
+        int random_variable = rand();
+        GenerateObstacle(&obstacle, random_variable);
+        Display(h, w, man, score, speed, obstacle);
+        MoveObstacle(&obstacle, h);
 
         while (kbhit() != 0)
         {                      // 如果它检测到有键盘敲击，返回非零。没有则返回 0
             char ch = getch(); // 有键盘敲击，我们获取是哪一个键
-
-            Sleep(1000); // 程序暂停 1000 毫秒
+            // Sleep(1000);       // 程序暂停 1000 毫秒
         }
         Sleep(100 - speed);
     }
@@ -97,21 +112,146 @@ void MvaddchCol(int y, int x, char ch)
     }
 }
 
-/**************************************************************************
- *void Runway(int y, int x)
+/*******************************************************************
+ *void MvaddchRow(int y, int x, char ch[])
  *
- *显示跑道界限和人物
+ *从 x,y处开始打印字符串ch[]
+ *
+ * 需要传入（期望出现的y坐标，期望出现的x坐标，打印字符串）
+ *
+ **********************************************************************/
+void MvaddchRow(int y, int x, char ch[])
+{
+    COORD co = (COORD){.X = x, .Y = y};
+    SetConsoleCursorPosition(consoleHandle, co); // 设置你的光标位置
+    printf("%s", ch);                            // 输出字符串
+}
+/***********************************************************************
+ *void MvaddchMiddle(int y, int x, int runway, char ch);
+ *
+ * 在指定跑道中间y处显示ch
+ *
+ * 需要传入（字符在的y，屏幕宽度，跑道代号，字符）
+ *************************************************************************/
+void MvaddchMiddle(int y, int x, int runway, char ch)
+{
+    switch (runway)
+    {
+    case 1:
+        Mvaddch(3 * y / 4, 7 * x / 18, ch); // 左道
+        break;
+    case 2:
+        Mvaddch(3 * y / 4, x / 2, ch); // 中道
+        break;
+    case 3:
+        Mvaddch(3 * y / 4, 11 * x / 18, ch); // 右道
+        break;
+    default:
+        printf("the MvaddchMiddle have wrong!\n");
+        break;
+    }
+}
+/**************************************************************************
+ *void Display(int y, int x)
+ *
+ *显示跑道界限、人物、得分、速度
  *
  *需要传入（屏幕高度，屏幕宽度）;详细参数可在函数内调整
  *
  * ************************************************************************/
-void Runway(int y, int x)
+void Display(int y, int x, int man, int score, int speed, Obstacle obstacle)
 {
+    // for (int i = 0; i <= y; i++) // 清屏
+    // {
+    //     printf("\n");
+    // }
+    system("cls");
+
     char CharOfRunway = '|'; // 显示跑道
     MvaddchCol(y, x / 3, CharOfRunway);
     MvaddchCol(y, 4 * x / 9, CharOfRunway);
     MvaddchCol(y, 5 * x / 9, CharOfRunway);
     MvaddchCol(y, 2 * x / 3, CharOfRunway);
 
-    Mvaddch(3 * y / 4, x / 2, '*'); // 显示人物
+    MvaddchMiddle(3 * y / 4, x, man, '*'); // 显示人物
+
+    for (int i = 1; i <= 3; i++) // 显示障碍物
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (obstacle.Down[i][j] != 0)
+            {
+                MvaddchMiddle(obstacle.Down[i][j], x, i, 'D');
+            }
+        }
+    }
+
+    MvaddchRow(y - 2, 2, "Score:"); // 显示得分
+    printf("%d", score);
+    MvaddchRow(y - 3, 2, "Speed:"); // 显示速度
+    printf("%d", score);
+}
+
+/*****************************************
+ *void InitObstacle(Obstacle *obstacle)
+ *
+ * 初始化obstacle
+ ******************************************/
+void InitObstacle(Obstacle *obstacle)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            obstacle->Down[i][j] = 0;
+        }
+    }
+}
+
+/*****************************************************
+ *void GenerateObstacle(Obstacle *obstacle, int random)
+ *
+ * 障碍物生成逻辑(缺少判断有路逻辑)
+ *
+ * 需要传入（障碍物结构体，随机数(未处理)）
+ *********************************************************/
+void GenerateObstacle(Obstacle *obstacle, int random)
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        if (obstacle->Down[i][0] == 0)
+        {
+            if (random % 17 == 1)
+            {
+                obstacle->Down[i][0] = 1;
+                random = rand();
+            }
+        }
+    }
+}
+
+/*****************************************************
+ *void MoveObstacle(Obstacle *obstacle)
+ *
+ *移动障碍物
+ *
+ *需要传入屏幕高度
+ *
+ *********************************************************/
+void MoveObstacle(Obstacle *obstacle, int y)
+{
+    for (int i = 1; i < 4; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (obstacle->Down[i][j] > y - 2)
+            {
+                obstacle->Down[i][j] = 0;
+            }
+            if (obstacle->Down[i][j] > 0)
+            {
+                obstacle->Down[i][j]++;
+            }
+        }
+    }
 }
